21127a0b81fa9850bd5a95605f34ea478b2c64a2
anirudh singh <anirudh@pop-os.localdomain>
2022-09-14 16:41:16 +0530

    [WIP] DPG with action control during simulation. Added Custom Reward function

--- inf.py
+++ inf.py
@@ -1,4 +1,3 @@
-from DPG import ENV_NAME, Agent
 import os
 import time
 import gym
@@ -19,21 +18,35 @@ class Agent_net(nn.Module):
     '''
     Estimates the best action which would have maximised q value for a given state
     '''
-    def __init__(self, n_obs: int, n_act: int, dev="cpu") -> None:
+    def __init__(self, n_obs: int, n_act: int, dev="cpu", control_size=6) -> None:
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_obs, 400),
+            nn.Linear(n_obs+control_size, 800),
             nn.LeakyReLU(0.1), 
-            nn.Linear(400, 300),
+            nn.Linear(800, 800),
             nn.LeakyReLU(0.1),
-            nn.Linear(300, n_act),
+            nn.Linear(800, 800),
+            nn.LeakyReLU(0.1),
+            nn.Linear(800, 800),
+            nn.LeakyReLU(0.1),
+            nn.Linear(800, n_act),
             nn.Tanh()
         )
         self.dev = dev
+        self.control_size = control_size
 
-    def forward(self, x):
+    def forward(self, x, control):
+        '''
+        control : int in [0, 5] to control bot motion direction
+        x : observation
+        '''
+        one_hot = torch.zeros((x.shape[0], self.control_size), device=self.dev, dtype=torch.float32)
+        for i in range(len(control)):
+            one_hot[i, control[i]] = 1
+        x = torch.cat((x, one_hot), dim=1)
         out = self.net(x)
         return out
+
 
 if __name__ == "__main__":
     spec = gym.envs.registry.spec(ENV_NAME)
@@ -42,15 +55,19 @@ if __name__ == "__main__":
     agent = Agent_net(
         env.observation_space.shape[0],
         env.action_space.shape[0],
+        dev=DEV
     ).to(DEV)
     state_dict = torch.load('model_0.pt')
     agent.load_state_dict(state_dict=state_dict['action_model'])
 
     is_done = False
     s = env.reset()
+    # env.minitaur._pybullet_client.resetBasePositionAndOrientation(
+        # env.minitaur.quadruped, [0, 0.5, 0.2], [0, 0, 0, 1])
+    control = [2]
     with torch.no_grad():
         while not is_done:
-            a = agent(torch.tensor(s, dtype=torch.float32).unsqueeze(0).to(DEV))
+            a = agent(torch.tensor(s, dtype=torch.float32).unsqueeze(0).to(DEV), control)
             a = a.squeeze().cpu().numpy()
             for i in range(5):
                 if not is_done:
